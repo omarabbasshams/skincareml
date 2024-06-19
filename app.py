63be43a85ced264fcc3630d760b7aaca702e5d09
omarabbasshams <omarabbasshams@gmail.com>
2024-06-19 13:13:36 +0300

    recomndation new

--- app.py
+++ app.py
@@ -1,30 +1,20 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from pydantic import BaseModel
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from predict import predict_image, read_imagefile
 from recommendation_service import get_recommendations
-from predict import predict, read_imagefile
 
 app = FastAPI()
 
-class RecommendationRequest(BaseModel):
-    answers: dict
-    issue: str
-
 @app.post("/predict/")
-async def predict_image(file: UploadFile = File(...)):
+async def predict(file: UploadFile = File(...)):
     image = read_imagefile(await file.read())
-    prediction = predict(image)
+    prediction = predict_image(image)
     return {"prediction": int(prediction)}
 
 @app.post("/recommend/")
-def recommend(request: RecommendationRequest):
-    try:
-        skin_type = request.answers.get('1')  # Assuming the skin type question has ID '1'
-        issue = request.issue
+async def recommend(skin_type: str, issue: str):
+    recommendations = get_recommendations(skin_type, issue)
+    return {"recommendations": recommendations}
 
-        if not skin_type or not issue:
-            raise HTTPException(status_code=400, detail="Missing skin type or issue information")
-
-        recommendations = get_recommendations(skin_type, issue)
-        return {"recommendations": recommendations}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
